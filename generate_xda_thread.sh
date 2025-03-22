9b75fbe91b9295063edab0a430bbd1294c092834
Joey Huab <jhuab@edukasyon.ph>
2025-03-23 03:00:10 +0900

    XDA: Drop Telegram and Twitter links

--- generate_xda_thread.sh
+++ generate_xda_thread.sh
@@ -230,16 +230,6 @@ generate_thread() {
             fi
         done
 
-        # Device support URL
-        while true; do
-            read -p "Device support URL: " device_support_url
-            if [[ $device_support_url =~ ^(https?://)?(www\.)?(discord\.com|discord\.gg|t\.me|telegram\.me|telegram\.org)(.*) ]]; then
-                break
-            else
-                echo -e "${RED}Invalid format. Please enter a valid URL from Discord or Telegram.${ENDCOLOR}"
-             fi
-        done
-
         cat << EOF > /tmp/generated_xda_thread.txt
 [CENTER]
 $manufacturer_name $device_name
@@ -253,7 +243,6 @@ $codenames
 [URL='https://xdaforums.com/m/anierinb.7125966/']Anierin Bliss[/URL] - Co-Founder/Co-Developer
 [URL='https://xdaforums.com/m/realakito.9008281/']Akito Mizukito[/URL] - Co-Founder/Project Manager
 
-[I]Reach us on Twitter! [URL='https://twitter.com/EvolutionXROM']@EvolutionXROM[/URL][/I]
 Check out our [URL='https://evolution-x.org/']website[/URL]!
 
 [IMG]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/features.png[/IMG]
@@ -294,7 +283,7 @@ fastboot reboot recovery
 2. Reboot & #KeepEvolving[/SIZE]
 [/SPOILER]
 
-[URL='$donation_url'][IMG width="200px" height="180px"]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/donate_to_me.png[/IMG][/URL][URL='https://discord.gg/3qbSZHx'][IMG width="200px" height="180px"]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/official_chat.png[/IMG][/URL][URL='$device_support_url'][IMG width="200px" height="180px"]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/device_support.png[/IMG][/URL][/CENTER]
+[URL='$donation_url'][IMG width="200px" height="180px"]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/donate_to_me.png[/IMG][/URL][URL='https://discord.gg/3qbSZHx'][IMG width="200px" height="180px"]https://raw.githubusercontent.com/Evolution-X/XDA/udc/assets/official_chat.png[/IMG][/URL][/CENTER]
 
 [TABLE]
 [TR]
